fccfcd8d952d318bb4636856c1680722e286f904
Damirkins <damirsariev470@gmail.com>
2024-06-17 17:05:53 +0300

    Поменял байт

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(requsts):
-    return HttpResponse("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Click me</a>")
+    return HttpResponse("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Здесь крылышки KFC</a>")
